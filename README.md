4828a7b60f291813c159821379e4279f0f724d9a
James Strachan <james.strachan@gmail.com>
2015-04-21 11:26:44 +0100

    added better docs

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
+fabric8/maven
+-------------
 
+This docker container will clone a git repository from **$SOURCE_URI** then run an arbitrary maven build from the **$MAVEN_COMMAND** returning the status code to indicate if the build worked of failed.
+
+### Environment variables
+
+-	SOURCE_URI - a URI to clone the git repository from
+-	SOURCE_REF - a reference to pass to Git for which commit to use (optional)
+-	CONTEXT_DIR - a subdirectory of the retrieved source to run the build fro
+-	MAVEN_COMMAND - the maven command line to run
+
+### Example
+
+```
+docker run -it -e SOURCE_URI=https://github.com/fabric8io/example-camel-cdi.git -e MAVEN_COMMAND="mvn install" fabric8-maven
+```
--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+# This is the image that clones a git repository and executes a maven build.
+# It expects a set of environment variables to parameterize the build:
+#   SOURCE_URI - a URI to clone the git repository from
+#   SOURCE_REF - a reference to pass to Git for which commit to use (optional)
+#   CONTEXT_DIR - a subdirectory of the retrieved source to run the build fro
+#   MAVEN_COMMAND - the maven command line to run
 
 if [ -z "${SOURCE_URI}" ]; then
   echo "No SOURCE_URI environment variable so cannot git clone the repository"
@@ -8,6 +13,7 @@ fi
 
 dirname=$(basename $SOURCE_URI)
 len=${#dirname}-4
+echo "cloning git repository: $SOURCE_URI"
 git clone $SOURCE_URI && cd $(echo "${dirname:0:$len}")
 
 if [ -z "${MAVEN_COMMAND}" ]; then
@@ -16,5 +22,5 @@ if [ -z "${MAVEN_COMMAND}" ]; then
 fi
 
 # now lets run the maven build
-echo "Running: $MAVEN_COMMAND"
+echo "running: $MAVEN_COMMAND"
 $MAVEN_COMMAND
